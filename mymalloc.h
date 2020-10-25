e40e37914ca1ab74e916f75ffff55d0d0a813cdc
Sagnik M <sagnim1337@gmail.com>
2020-10-25 12:51:43 -0400

    Update mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -3,16 +3,28 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <math.h>
 
 #define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
 #define free( x ) myfree( x, __FILE__, __LINE__ )
+#define MYSIZE 4096
 
-static char myblock[];
+static char myblock[MYSIZE];
 
-void *mymalloc(unsigned size, char* file, int linenum);
+struct entry
+{
+	int isfree; //0 - not free
+	int entrySize;
+	struct entry *prev;
+	struct entry *next;
+};
+
+static void *memEntries[MYSIZE/sizeof(struct entry) + 1] = {0};
+
+void *mymalloc(size_t size, char* file, int linenum);
 void myfree(void* p, char* file, int linenum);
 
-#endif /* _mymalloc_H_ */
+#endif // _mymalloc_H_
